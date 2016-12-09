0cd7b3a443a1ca67536c133fc8c33f6c5c7a0c5a
Sheng Wei <capwone@gmail.com>
2016-12-09 15:14:12 -0500

    add experiment data, need to plot the graph...

--- prepare.sh
+++ prepare.sh
@@ -4,9 +4,9 @@
 #
 #
 
-NUM_OF_SERVERS=4
+NUM_OF_SERVERS=32
 PORT_BASE=5000
-for i in `seq 0 3`; do
+for i in `seq 0 $((NUM_OF_SERVERS-1))`; do
 	taskset -c $((1+$i)) redis-3.2.3/src/redis-server --daemonize yes --port $((5000+$i)) --protected-mode no & 
 	wait
 done
